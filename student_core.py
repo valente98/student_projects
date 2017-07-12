4fca74d1a036345cb2ab0d3d6e1003284421cdbd
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-12 10:54:12 -0500

    added a function specificly to the instructor

--- student_core.py
+++ student_core.py
@@ -2,4 +2,8 @@ import datetime
 
 def intro():
     msg = input('Welcome to BASE CAMP CODING ACADEMY!\n Please type in your first and last name.: ')
+    return msg
 
+def teacher():
+    msg = input('Which student would you like to check up on?: ')
+    return msg
