3f46d7a5c83fb16c5fe00b0cdeb2dfe4eb8f0b33
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-13 08:37:34 -0500

    created some functions to open files

--- disk.py
+++ disk.py
@@ -6,3 +6,21 @@ def Check_in(msg):
     message = '\n{}, {}'.format(msg, current)
     with open('checkins.txt', 'a') as file:
         file.write(message)
+
+def instructor():
+    name = []
+    with open('checkins.txt', 'r') as file:
+        file.readline()
+        lines = file.readlines()
+    for line in lines:
+        split_string = line.strip().split(', ')
+        name.append([split_string[0], split_string[1]])
+    return name
+
+def roster():
+    names = []
+    with open('roster.txt', 'r') as file:
+        file.readline()
+        lines = file.readlines()
+    for line in lines:
+        
