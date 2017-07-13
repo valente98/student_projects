09e0aa172d6e6c7ac2372745f391a0637a94fd7d
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-13 08:49:30 -0500

    deleted the function and put replaced them in the shell

--- student_core.py
+++ student_core.py
@@ -1,9 +1,23 @@
 import datetime
+import disk
+def check_up_name(name):
+    list_name = disk.instructor()
+    check = ''
+    for item in list_name:
+        if name == item[0]:
+            check += ('\n'+str(item))
+    print(check)
 
-def intro():
-    msg = input('Welcome to BASE CAMP CODING ACADEMY!\n Please type in your first and last name.: ')
-    return msg
+def check_up_date(date):
+    list_date = disk.instructor()
+    check = ''
+    for item in list_date:
+        if item[1].startswith(date):
+            check += ('\n'+str(item))
+    print(check)
 
-def teacher():
-    msg = input('Which student would you like to check up on?: ')
-    return msg
+def check_up_all():
+    print('Here is the whole list \n')
+    for_all = disk.instructor()
+    for item in for_all:
+        print('\n'+str(item))
